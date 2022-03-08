3da806bd930ca64f9c937e50c6ae2f707c86a128
Rogri <rogri0307@hotmail.com>
2022-03-08 15:02:38 -0300

    my first update

--- app/src/main/java/com/rogriaqua/aquarium/FishiesFragment.kt
+++ app/src/main/java/com/rogriaqua/aquarium/FishiesFragment.kt
@@ -1,4 +1,22 @@
 package com.rogriaqua.aquarium
 
-class FishesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rogriaqua.aquarium.databinding.FragmentFishiesBinding
+
+class FishiesFragment: Fragment() {
+
+    private lateinit var binding: FragmentFishiesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFishiesBinding.inflate(inflater)
+        return binding.root
+    }
 }
