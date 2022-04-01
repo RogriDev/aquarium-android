e85de6aa99213d800d60fff61bbf07a7b6c407e4
Rogri <rogri0307@hotmail.com>
2022-03-31 23:23:09 -0300

    new update implementing 31/03/2022

--- app/src/main/java/com/rogriaqua/aquarium/Fishies.kt
+++ app/src/main/java/com/rogriaqua/aquarium/Fishies.kt
@@ -2,4 +2,9 @@ package com.rogriaqua.aquarium
 
 import androidx.annotation.DrawableRes
 
-data class Fishies(@DrawableRes val image: Int, val name: String, val water: String)
+data class Fishies(
+    @DrawableRes val image: Int,
+    val name: String,
+    val water: String,
+    @DrawableRes val favorite: Int
+)
--- app/src/main/java/com/rogriaqua/aquarium/FishiesActivity.kt
+++ app/src/main/java/com/rogriaqua/aquarium/FishiesActivity.kt
@@ -4,23 +4,26 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.rogriaqua.aquarium.databinding.ActivityFishiesBinding
 
 class FishiesActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityFishiesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fishies)
+        binding = ActivityFishiesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val onClickListener = ItemClickListener { fishies ->
             Toast.makeText(this, "'${fishies.name} clicked", Toast.LENGTH_SHORT).show()
         }
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
         val adapter = FishiesAdapter(FishiesData().loadFishies(), onClickListener)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
 
     }
 }
